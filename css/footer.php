223e36b0dbee177d4198a0e0159a35dc8d7e2042
AOT9707 <giby-torres@hotmail.com>
2018-11-22 12:09:09 -0600

    Se modifico index con footer

---
+++ css/footer.php
@@ -0,0 +1,8 @@
+<PRE>
+</PRE>
+<CENTER>
+<A HREF="http://localhost/saltarinWeb/css/info.php">Derechos Reservados</A> 2018 By SalarinGroup<BR>
+Facebook: <I>SaltarinGroupMX</I><BR>
+Alguna duda o comentario, favor de escribirnos a: <B>saltaringroupmx@gmail.com</B><BR>
+</CENTER>
+</BODY>
--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php session_start();
 
 require 'view/index.view.php';
+require 'css/footer.php';
+
  ?>
